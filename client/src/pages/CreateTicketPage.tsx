654dbbdb62a70e8d2af2d12dc339510934d3630f
Felipe Lagos <felagossanchez@gmail.com>
2024-01-19 12:28:49 -0300

    Add new page exports

---
+++ client/src/pages/CreateTicketPage.tsx
@@ -0,0 +1,7 @@
+export const CreateTicketPage = () => {
+	return (
+		<div>
+			<h1>Create Ticket Page</h1>
+		</div>
+	)
+}
---
+++ client/src/pages/QueuePage.tsx
@@ -0,0 +1,7 @@
+export const QueuePage = () => {
+	return (
+		<div>
+			<h1>Queue Page</h1>
+		</div>
+	)
+}
