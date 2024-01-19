f9c6b3648a93d584410fb7f356eb74fb68b229ac
Felipe Lagos <felagossanchez@gmail.com>
2024-01-19 13:47:29 -0300

    Add login form to LogInPage

--- client/src/pages/LogInPage.tsx
+++ client/src/pages/LogInPage.tsx
@@ -1,7 +1,46 @@
+import { Button, Form, Input } from 'antd';
+
+type FieldType = {
+	username?: string;
+	password?: string;
+};
+
+const onFinish = (values: FieldType) => {
+  console.log('Success:', values);
+};
+
 export const LogInPage = () => {
 	return (
-		<div>
-			<h1>Log In Page</h1>
-		</div>
+		<Form
+			name="basic"
+			labelCol={{ span: 8 }}
+			wrapperCol={{ span: 16 }}
+			style={{ maxWidth: 600 }}
+			initialValues={{ remember: true }}
+			onFinish={onFinish}
+			autoComplete="off"
+		>
+			<Form.Item<FieldType>
+				label="Username"
+				name="username"
+				rules={[{ required: true, message: 'Please input your username!' }]}
+			>
+				<Input />
+			</Form.Item>
+
+			<Form.Item<FieldType>
+				label="Password"
+				name="password"
+				rules={[{ required: true, message: 'Please input your password!' }]}
+			>
+				<Input.Password />
+			</Form.Item>
+
+			<Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+				<Button type="primary" htmlType="submit">
+					Submit
+				</Button>
+			</Form.Item>
+		</Form>
 	)
 }
