93b92265fa26027ee1058dc7e31b3f38017d92fe
Felipe Lagos <felagossanchez@gmail.com>
2024-01-19 10:38:03 -0300

    Update Layout component styles and functionality

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -1,10 +1,71 @@
-import { Layout as LayoutAnt } from "antd";
+import { useState } from "react";
+import { Layout as LayoutAnt, Menu, Button, theme } from 'antd';
 import { Outlet } from "react-router-dom";
+import {
+	MenuFoldOutlined,
+	MenuUnfoldOutlined,
+	UploadOutlined,
+	UserOutlined,
+	VideoCameraOutlined,
+} from '@ant-design/icons';
 
 import './Layout.scss';
 
-export const Layout =  () => (
-	<LayoutAnt className="layout">
-		<Outlet />
-	</LayoutAnt>
-)
+const { Header, Sider, Content } = LayoutAnt;
+
+export const Layout = () => {
+	const [collapsed, setCollapsed] = useState(false);
+
+	const {
+		token: { colorBgContainer, borderRadiusLG },
+	} = theme.useToken();
+
+	return (
+		<LayoutAnt>
+			<Sider trigger={null} collapsible collapsed={collapsed}>
+				<div className="demo-logo-vertical" />
+				<Menu
+					theme="dark"
+					mode="inline"
+					defaultSelectedKeys={['1']}
+					items={[
+						{
+							key: '1',
+							icon: <UserOutlined />,
+							label: 'nav 1',
+						},
+						{
+							key: '2',
+							icon: <VideoCameraOutlined />,
+							label: 'nav 2',
+						},
+						{
+							key: '3',
+							icon: <UploadOutlined />,
+							label: 'nav 3',
+						},
+					]}
+				/>
+			</Sider>
+			<LayoutAnt>
+				<Header style={{ padding: 0, background: colorBgContainer }}>
+					<Button
+						type="text"
+						icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+						onClick={() => setCollapsed(!collapsed)}
+						className="layout__collapsible"
+					/>
+				</Header>
+				<Content
+					className="layout__content"
+					style={{
+						background: colorBgContainer,
+						borderRadius: borderRadiusLG,
+					}}
+				>
+					<Outlet />
+				</Content>
+			</LayoutAnt>
+		</LayoutAnt>
+	)
+}
