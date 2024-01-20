f55a82ee0bb41d822e5c72d5c6b1a0c588ee7b85
Felipe Lagos <felagossanchez@gmail.com>
2024-01-20 12:43:48 -0300

    Add exit button to DesktopPage

--- client/src/pages/DesktopPage.tsx
+++ client/src/pages/DesktopPage.tsx
@@ -1,16 +1,31 @@
-import { Col, Row, Typography } from "antd"
+import { CloseCircleOutlined } from "@ant-design/icons"
+import { Button, Col, Row, Typography } from "antd"
+
+import "./DesktopPage.scss"
 
 export const DesktopPage = () => {
+
+	const handleExit = () => { }
+
 	return (
 		<>
-			<Row>
-				<Col span={20}>
+			<Row className="desktop">
+				<Col xl={12} md={24} sm={24}>
 					<Typography.Title level={2}>Desktop</Typography.Title>
 					<Typography.Text>You are working in desktop: </Typography.Text>
 					<Typography.Text type="success">No. 55</Typography.Text>
 				</Col>
-				<Col span={4}>
-					
+				<Col xl={12} md={24} sm={24} className="right">
+					<Button
+						type="primary"
+						danger
+						icon={<CloseCircleOutlined />}
+						shape="round"
+						onClick={handleExit}
+						size="large"
+					>
+						Exit
+					</Button>
 				</Col>
 			</Row>
 		</>
