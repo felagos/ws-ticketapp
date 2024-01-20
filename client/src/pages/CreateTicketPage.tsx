ae8ed67fe6867647c28c8fcf60952032de2e6f2c
Felipe Lagos <felagossanchez@gmail.com>
2024-01-20 13:48:41 -0300

    Add new ticket creation functionality and center alignment utility class

--- client/src/pages/CreateTicketPage.tsx
+++ client/src/pages/CreateTicketPage.tsx
@@ -1,7 +1,36 @@
+import { DownloadOutlined } from "@ant-design/icons";
+import { Button, Col, Row, Typography } from "antd"
+
+const { Text, Title } = Typography;
+
 export const CreateTicketPage = () => {
+	const handleNewTicket = () => { }
+
 	return (
-		<div>
-			<h1>Create Ticket Page</h1>
-		</div>
+		<>
+			<Row>
+				<Col span={14} offset={6} className="u-center">
+					<Title level={3}>Press to create a new ticket</Title>
+					<Button
+						type="primary"
+						shape="round"
+						icon={<DownloadOutlined />}
+						size="large"
+						onClick={handleNewTicket}
+					>
+						New Ticket
+					</Button>
+				</Col>
+			</Row>
+			<Row style={{ marginTop: 50 }}>
+				<Col span={14} offset={6} className="u-center">
+					<Text>Your number</Text>
+					<br />
+					<Text type="success" style={{ fontSize: 55 }}>
+						55
+					</Text>
+				</Col>
+			</Row>
+		</>
 	)
 }
