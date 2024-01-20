17355c475211746fc9bf6536991322ca3f65c16e
Felipe Lagos <felagossanchez@gmail.com>
2024-01-20 12:07:22 -0300

    Refactor DesktopPage component to include typography and layout components

--- client/src/pages/DesktopPage.tsx
+++ client/src/pages/DesktopPage.tsx
@@ -1,7 +1,18 @@
+import { Col, Row, Typography } from "antd"
+
 export const DesktopPage = () => {
 	return (
-		<div>
-			<h1>Desktop Page</h1>
-		</div>
+		<>
+			<Row>
+				<Col span={20}>
+					<Typography.Title level={2}>Desktop</Typography.Title>
+					<Typography.Text>You are working in desktop: </Typography.Text>
+					<Typography.Text type="success">No. 55</Typography.Text>
+				</Col>
+				<Col span={4}>
+					
+				</Col>
+			</Row>
+		</>
 	)
 }
