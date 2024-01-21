e1d99f6c6f77901309a8cd79ec91824ca39cd6f1
Felipe Lagos <felagossanchez@gmail.com>
2024-01-21 14:00:44 -0300

    Add hideMenu and toggleMenu to UiState

--- client/src/store/useUiStore.ts
+++ client/src/store/useUiStore.ts
@@ -1,8 +1,13 @@
 import { StateCreator, create } from "zustand";
 
-interface UiState {}
+interface UiState {
+	hideMenu: boolean;
+	toggleMenu: (hideMenu: boolean) => void;
+}
 
 const storeApi: StateCreator<UiState> = (set) => ({
+	hideMenu: false,
+	toggleMenu: (hideMenu: boolean) => set({ hideMenu }),
 });
 
 export const useAuthStore = create<UiState>()(storeApi);
