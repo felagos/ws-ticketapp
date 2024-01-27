c575f89c6d73649f285e11cae6a3e31417be58a2
Felipe Lagos <felagossanchez@gmail.com>
2024-01-27 18:04:39 -0300

    Update DesktopPage to display user's agent and desktop number

--- client/src/pages/DesktopPage.tsx
+++ client/src/pages/DesktopPage.tsx
@@ -1,9 +1,11 @@
 import { CloseCircleOutlined, RightOutlined } from "@ant-design/icons"
 import { Button, Col, Divider, Row, Typography } from "antd"
+import { useUser } from "../hooks"
 
 import "./DesktopPage.scss"
 
 export const DesktopPage = () => {
+	const { user } = useUser();
 
 	const handleExit = () => { }
 
@@ -13,9 +15,9 @@ export const DesktopPage = () => {
 		<>
 			<Row className="desktop">
 				<Col xl={12} md={24} sm={24}>
-					<Typography.Title level={2}>Desktop</Typography.Title>
+					<Typography.Title level={2}>{user.agent}</Typography.Title>
 					<Typography.Text>You are working in desktop: </Typography.Text>
-					<Typography.Text type="success">No. 55</Typography.Text>
+					<Typography.Text type="success">No. {user.desktop}</Typography.Text>
 				</Col>
 				<Col xl={12} md={24} sm={24} className="right">
 					<Button
@@ -36,7 +38,7 @@ export const DesktopPage = () => {
 			<Row>
 				<Col>
 					<Typography.Title level={3}>You are attending ticket: </Typography.Title>
-					<Typography.Text className="u-font-30" type="danger">No. 55</Typography.Text>
+					<Typography.Text className="u-font-30" type="danger">No. {user.desktop}</Typography.Text>
 				</Col>
 			</Row>
 
