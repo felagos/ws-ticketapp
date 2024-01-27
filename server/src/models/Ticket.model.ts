3b21efad429a7cf1f07fa25a39cbc35c15923405
Felipe Lagos <felagossanchez@gmail.com>
2024-01-27 18:37:51 -0300

    Delete unused models directory

---
+++ server/src/models/Ticket.model.ts
@@ -0,0 +1,28 @@
+import { v4 as uuid } from 'uuid'
+
+export class TicketModel {
+
+	private readonly id = uuid();
+
+	constructor(
+		private readonly numberTicket: number,
+		private readonly desktop: number,
+		private readonly agent: string) { }
+
+	getId() {
+		return this.id;
+	}
+
+	getNumberTicket() {
+		return this.numberTicket;
+	}
+
+	getDesktop() {
+		return this.desktop;
+	}
+
+	getAgent() {
+		return this.agent;
+	}
+
+}
